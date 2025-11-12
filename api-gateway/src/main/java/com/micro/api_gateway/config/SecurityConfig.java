684f479ea1e83dabbaca891dc0349e08880c9f42
sotheakam <sotheakam201@gmail.com>
2025-11-12 15:09:32 +0700

    update api-gateway

--- api-gateway/src/main/java/com/micro/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/micro/api_gateway/config/SecurityConfig.java
@@ -3,12 +3,12 @@ package com.micro.api_gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
