171d0fe50a6e35c95d4b65439bea7cf9e8ac3a68
sotheakam <sotheakam201@gmail.com>
2025-11-13 13:11:36 +0700

    add

--- customer/src/main/java/com/micro/customer/controller/CustomerController.java
+++ customer/src/main/java/com/micro/customer/controller/CustomerController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class CustomerController {
 
     @Autowired
-    private  ProductClient productClient;
+    private ProductClient productClient;
 
     @Value("${customer.service.greeting}")
     private String greeting;
